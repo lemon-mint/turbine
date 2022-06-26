312d0bb6ff30a51216283eb3c148d6df6be2e621
lemon-mint <ice.lemons.mint@gmail.com>
2022-06-26 11:24:17 +0900

    Schedule

--- wheel.go
+++ wheel.go
@@ -1,14 +1,39 @@
 package turbine
 
-import "time"
+import (
+	"errors"
+	"sync"
+	"time"
+)
+
+type TurbineState uint32
+
+const (
+	TurbineStateStopped TurbineState = iota
+	TurbineStateIdle
+	TurbineStateRunning
+	TurbineStateStopping
+)
 
 type Turbine struct {
-	unit time.Duration
+	blades       []blade
+	cursor       int
+	tasksCounter uint64
 
-	blades []blade
+	unit  time.Duration
+	state TurbineState
 }
 
 type blade struct {
+	mu sync.Mutex
+
+	tasks []func()
+}
+
+func (b *blade) load(f func()) {
+	b.mu.Lock()
+	b.tasks = append(b.tasks, f)
+	b.mu.Unlock()
 }
 
 func NewTurbine(unit time.Duration, size int) *Turbine {
@@ -18,6 +43,15 @@ func NewTurbine(unit time.Duration, size int) *Turbine {
 	}
 }
 
-func (t *Turbine) schedule() {
+var ErrOutOfRange = errors.New("out of range")
 
+func (t *Turbine) Schedule(after time.Duration, f func()) error {
+	if after > t.unit*time.Duration(len(t.blades)-1) {
+		return ErrOutOfRange
+	}
+
+	targetBlade := (t.cursor + int(after/t.unit)) % len(t.blades)
+	t.blades[targetBlade].load(f)
+
+	return nil
 }
